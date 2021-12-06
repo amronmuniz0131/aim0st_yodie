d3178663c096b6fd7c92dbdec944f560195b4cd6
amronmuniz0131 <49471600+amronmuniz0131@users.noreply.github.com>
2021-12-07 02:17:40 +0800

    tables working
    tables for total SLP and average gain are now working.

--- client/axie_management/src/Total_slp.js
+++ client/axie_management/src/Total_slp.js
@@ -7,7 +7,7 @@ function Total_slp(a) {
   const [totalSlp, getSlp] = useState([]);
   const [overAll, getOverAll] = useState([]);
 
-    const userId = ['0x26d252724d08a30151ab5c87bd6b4fb5eadb1500','0xcc72a2ae194ce933bbd8893057de23fcd7f457ef', '0x7b661b64a72c42ae2f7552385dc27064a234691d'];
+    const userId = ['0xdcb426edd4ffae49f1379d64afe7dfd66c48fde6','0x26d252724d08a30151ab5c87bd6b4fb5eadb1500','0xcc72a2ae194ce933bbd8893057de23fcd7f457ef', '0x7b661b64a72c42ae2f7552385dc27064a234691d'];
     const total = []; 
 
 
@@ -24,130 +24,48 @@ function Total_slp(a) {
       }
     }
     
-    //console.log(userId[0]);
-
-    
       useEffect(()=>{
         for (let index = 0; index < userId.length; index++) {
           axios.request(getTotal(userId[index], index))
           .then(
             (response) => {
-              total.push(response.data.slp);
+              total.push(response.data);
               //console.log(total);
               getOverAll(total)
               //console.log(response.data.slp);
 
-              getSlp(response.data.slp);
+              getSlp(response.data);
 
           }).catch(function (error) {
             console.error(error);
           })
         }
       },[]);
-
-      console.log(overAll);
-
-      
-       //   a = userId.map(b =>
-    //   {
-    //     return 
-        
-    // } )
-    
-      // let b = {
-      //   method: 'GET',
-      //   url: 'https://axie-infinity.p.rapidapi.com/get-update/0x26d252724d08a30151ab5c87bd6b4fb5eadb1500',
-      //   params: {id: userId.userId},
-      //   headers: {
-      //     'x-rapidapi-host': 'axie-infinity.p.rapidapi.com',
-      //     'x-rapidapi-key': '01ff0af53bmsh7c2736122730407p1465c3jsn237847b0957e'
-      //   }
-      // }
-
-      // let c = {
-      //   method: 'GET',
-      //   url: 'https://axie-infinity.p.rapidapi.com/get-update/0xcc72a2ae194ce933bbd8893057de23fcd7f457ef',
-      //   params: {id: userId.userId},
-      //   headers: {
-      //     'x-rapidapi-host': 'axie-infinity.p.rapidapi.com',
-      //     'x-rapidapi-key': '01ff0af53bmsh7c2736122730407p1465c3jsn237847b0957e'
-      //   }
-      // }
-
-      // let d = {
-      //   method: 'GET',
-      //   url: 'https://axie-infinity.p.rapidapi.com/get-update/0x7b661b64a72c42ae2f7552385dc27064a234691d',
-      //   params: {id: userId.userId},
-      //   headers: {
-      //     'x-rapidapi-host': 'axie-infinity.p.rapidapi.com',
-      //     'x-rapidapi-key': '01ff0af53bmsh7c2736122730407p1465c3jsn237847b0957e'
-      //   }
-      // }
-
-        // axios.request(b)
-        //   .then(
-        //     (response) => {
-        //       console.log(response.data.slp);
-        //     getSlp(response.data.slp);
-        //   }).catch(function (error) {
-        //     console.error(error);
-        //   })
-
-        //   axios.request(c)
-        //   .then(
-        //     (response) => {
-        //       console.log(response.data.slp);
-        //     getSlp(response.data.slp);
-        //   }).catch(function (error) {
-        //     console.error(error);
-        //   })
-
-        //   axios.request(d)
-        //   .then(
-        //     (response) => {
-        //       console.log(response.data.slp);
-        //     getSlp(response.data.slp);
-        //   }).catch(function (error) {
-        //     console.error(error);
-        //   })
-
-        //a.map(c=> { 
-        //   return (
-          
-          
-
-          
-          
-        // )
-        
-      //})
-        //eslint-disable-next-line
+      console.log(overAll)
     return (
       
       <div>
           <table className="table__border">
             <thead>
               <tr>  
+                <th>Name</th>
                 <th>Today</th>
                 <th>Average</th>
                 <th>Yesterday</th>
+                <th>Adventure</th>
                 <th>Total SLP</th>
               </tr>
             </thead>
-          
-          <tbody>
-            {
-               overAll.forEach((element) => {
-                console.log(element['average']);
-               return <tr>
-                  <td>{element['todaySoFar']}</td>
-                  <td>{element['average']}</td>
-                  <td>{element['yesterdaySLP']}</td>
-                  <td>{element['total']}</td>
-                </tr>
-               })
-            }
-          </tbody>
+                 {overAll.map(post =>
+                    <tbody> 
+                      <td>{post.leaderboard.name}</td>
+                      <td>{post.slp.todaySoFar}</td>
+                      <td>{post.slp.average}</td> 
+                      <td>{post.slp.yesterdaySLP}</td> 
+                      <td>{post.adventure.gained_slp}/{post.adventure.max_slp}</td>
+                      <td>{post.slp.total}</td> 
+                    </tbody>)}
+
          
           </table>
         
